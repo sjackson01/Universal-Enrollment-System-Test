93f7a1447667e5ba3dcfe915287b4696e3596b55
sjackson01 <stevenpjackson0@gmail.com>
2020-03-31 14:26:56 -0500

    feat (Program.cs): remove procedural connection logic and replace with Connection instance feat (Connection.cs): create class that opens stream to external data source

---
+++ UES/Connection.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.IO;
+
+
+namespace UES
+{
+    class Connection
+    {
+
+        public readonly string DataSource;
+
+        public Connection(string dataSource)
+        { 
+            DataSource = dataSource;
+        }
+
+        public string GetData(string dataSource) 
+        {
+            WebRequest request = WebRequest.Create(dataSource);
+            WebResponse response = (WebResponse)request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream, System.Text.Encoding.GetEncoding("utf-8"));
+            string json = reader.ReadToEnd();
+            reader.Close();
+            return json; 
+        }
+        
+    }
+}
--- UES/Program.cs
+++ UES/Program.cs
@@ -10,33 +10,11 @@ namespace UES
     class Program
     {
         static void Main(string[] args)
-        {
-
-            // JSON location 
-            // Place in the xml config file 
-            string apiUri = "https://jsonplaceholder.typicode.com/todos/1";
-
-            // Send web service request
-            // Original Code: HttpWebRequest request = (HttpWebRequest)WebRequest.Create(apiUri);
-            WebRequest request = WebRequest.Create(apiUri);
-
-            // Get response
-            // Originial Code: HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            WebResponse response = (WebResponse)request.GetResponse();
-
-            // Create stream
-            Stream responseStream = response.GetResponseStream();
-
-            // Set up the stream reader
-            // Talk to Jeff about encoding 
-            StreamReader readerStream = new StreamReader(responseStream, System.Text.Encoding.GetEncoding("utf-8"));
-
-            // Transfer to a string 
-            string json = readerStream.ReadToEnd();
-
-            // Close the stream
-            readerStream.Close();
-
+        {   
+            // Connect to data source
+            Connection JsonLink = new Connection("https://jsonplaceholder.typicode.com/todos/1");
+            string json = JsonLink.GetData(JsonLink.DataSource);
+            
             // Convert into JSON object 
             var jo = JObject.Parse(json);
 
